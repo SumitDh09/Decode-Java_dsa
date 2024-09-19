7a2ecd7518b8cdc4aec1a5e75ead66917a6d04fe
SumitDhondikar <dhondikarsumit@gmail.com>
2024-09-19 12:55:29 +0530

    All java Programs

--- src/Recursion/PreInPost.java
+++ src/Recursion/PreInPost.java
@@ -4,11 +4,11 @@ public class PreInPost {
     public static void pip(int n) {
         if (n == 0)
             return;
-        System.out.println(n);
+        System.out.println(n); //pre
         pip(n - 1);
-        System.out.println(n);
+        System.out.println(n); //in
         pip(n - 1);
-        System.out.println(n);
+        System.out.println(n); //post
     }
 
     public static void main(String[] args) {
