950088c480e9b824a6ee565bcb0bdf7f26c46d29
SumitDhondikar <dhondikarsumit@gmail.com>
2024-09-24 21:33:27 +0530

    All java Programs

--- src/Strings/MergeStringAlt.java
+++ src/Strings/MergeStringAlt.java
@@ -5,18 +5,17 @@ class MergeStringAlt {
             String mergedResult = merger.mergeStrings("hello", "world");
             System.out.println("Merged result: " + mergedResult);
         }
+    public String mergeAlternately(String k1, String k2) {
+    // dekho bhai yha word1 ko bana diya h k1
 
-    //Leetcode 1768:--  You are given two strings word1 and word2.
-    //Merge the strings by adding letters in alternating order,starting with word1.
-    //If a string is longer than the other, append the additional letters onto the end of the merged string.
-public String mergeAlternately(String k1, String k2) {
     final int n = Math.min(k1.length(), k2.length());
         StringBuilder sb = new StringBuilder();
             for (int i = 0; i < n; ++i) {
                 sb.append(k1.charAt(i));
                 sb.append(k2.charAt(i));
             }
-            return sb.append(k1.substring(n)).append(k2.substring(n)).toString();
+    //Or bhai yaha word2 ko bana diya h K2
+    return sb.append(k1.substring(n)).append(k2.substring(n)).toString();
         }
 
         // New method for merging strings alternately
@@ -32,8 +31,11 @@ public String mergeAlternately(String k1, String k2) {
             }
             return result.append(s1.substring(n)).append(s2.substring(n)).toString();
         }
-}
 
+}
+//Leetcode 1768:--  You are given two strings word1 and word2.
+//Merge the strings by adding letters in alternating order,starting with word1.
+//If a string is longer than the other, append the additional letters onto the end of the merged string.
 
 
 
