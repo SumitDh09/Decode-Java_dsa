5f2576ec952985b76e1644473978e11a9462c04f
SumitDhondikar <dhondikarsumit@gmail.com>
2024-09-25 16:27:33 +0530

    All java Programs

--- src/QuizApplication/QuestionService.java
+++ src/QuizApplication/QuestionService.java
@@ -8,9 +8,13 @@ public class QuestionService {
 
     public QuestionService() {
         questions[0] = new Question(1, "size of int", "2", "6", "4", "8", "4");
+
         questions[1] = new Question(2, "size of double", "2", "6", "4", "8", "8");
+
         questions[2] = new Question(3, "size of char", "2", "6", "4", "8", "2");
+
         questions[3] = new Question(4, "size of long", "2", "6", "4", "8", "8");
+
         questions[4] = new Question(5, "size of boolean", "1", "2", "4", "8", "1");
 
 
@@ -20,12 +24,19 @@ public class QuestionService {
         int i = 0;
         for (Question q : questions) {
             System.out.println("Question no. : " + q.getId());
+
             System.out.println(q.getQuestion());
+
             System.out.println(q.getOpt1());
+
             System.out.println(q.getOpt2());
+
             System.out.println(q.getOpt3());
+
             System.out.println(q.getOpt4());
+
             Scanner sc = new Scanner(System.in);
+
             selection[i] = sc.nextLine();
             i++;
 
