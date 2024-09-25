076940fa12392ff6286a9212d8c130aecebc3f59
SumitDhondikar <dhondikarsumit@gmail.com>
2024-09-25 15:34:06 +0530

    All java Programs

--- src/Strings/Stringbuilderfunc.java
+++ src/Strings/Stringbuilderfunc.java
@@ -3,7 +3,7 @@ package Strings;
 public class Stringbuilderfunc {
 
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("abcxduhius");
+        StringBuilder sb = new StringBuilder("abcxd" + "uhius");
         StringBuilder tb = new StringBuilder("chomu");
         System.out.println(sb.length());
         System.out.println(sb.compareTo(tb));
