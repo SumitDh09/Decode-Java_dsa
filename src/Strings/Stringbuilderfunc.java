ed35dec0d0d68b9c63e249109c1916de53ce6401
SumitDhondikar <dhondikarsumit@gmail.com>
2024-09-25 12:05:43 +0530

    All java Programs

--- src/Strings/Stringbuilderfunc.java
+++ src/Strings/Stringbuilderfunc.java
@@ -4,7 +4,7 @@ public class Stringbuilderfunc {
 
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("abcxduhius");
-        StringBuilder tb = new StringBuilder("madhav");
+        StringBuilder tb = new StringBuilder("chomu");
         System.out.println(sb.length());
         System.out.println(sb.compareTo(tb));
         System.out.println(sb.reverse());
