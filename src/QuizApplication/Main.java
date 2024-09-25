0aba89fc3e10925745aaaf3ee029661d86639f59
SumitDhondikar <dhondikarsumit@gmail.com>
2024-09-25 16:03:40 +0530

    All java Programs

---
+++ src/QuizApplication/Main.java
@@ -0,0 +1,10 @@
+package QuizApplication;
+
+public class Main {
+    public static void main(String[] args) {
+
+        QuestionService questionService = new QuestionService();
+        questionService.playQuiz();
+        questionService.printScore();
+    }
+}
