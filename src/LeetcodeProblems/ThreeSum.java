fbff2b5a5e82c013f6d39ef98b1c5e455decef94
SumitDhondikar <dhondikarsumit@gmail.com>
2024-10-27 20:54:57 +0530

    All java Programs

---
+++ src/LeetcodeProblems/ThreeSum.java
@@ -0,0 +1,44 @@
+package LeetcodeProblems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class ThreeSum {
+    public static void main(String[] args) {
+        ThreeSum obj = new ThreeSum();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = obj.threeSum(nums);
+        System.out.println("Triplets that sum to zero: " + result);
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3)
+            return new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            // Choose nums[i] as the first number in the triplet, then search the
+            // remaining numbers in [i + 1, n - 1].
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                final int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
+                    while (l < r && nums[l] == nums[l - 1])
+                        ++l;
+                    while (l < r && nums[r] == nums[r + 1])
+                        --r;
+                } else if (sum < 0) {
+                    ++l;
+                } else {
+                    --r;
+                }
+            }
+        }
+        return ans;
+    }
+}
