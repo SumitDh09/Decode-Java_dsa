acad36a3da5a6c17a04b348b54de23276792a4a7
SumitDhondikar <dhondikarsumit@gmail.com>
2024-11-11 20:40:26 +0530

    All java Programs

---
+++ src/Basics/ABC.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class ABC {
+    public static void main(String[] args) {
+
+        for(char chh = 'A'; chh <='Z';chh++){
+            System.out.print(chh + " ");
+        }
+        for (char ch = 'a'; ch <='z'; ch++){
+            System.out.print(ch + " ");
+        }
+    }
+}
