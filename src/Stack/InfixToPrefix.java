762e4572ee6dbf098ea98982a86cc4f9486f3310
SumitDhondikar <dhondikarsumit@gmail.com>
2024-11-15 14:08:45 +0530

    All java Programs

---
+++ src/Stack/InfixToPrefix.java
@@ -0,0 +1,59 @@
+package Stack;
+import java.util.Stack;
+
+public class InfixToPrefix {
+    public static void main(String[] args) {
+        String str = "8-(5+3)*(4/6)";
+        System.out.println("Infix Expression: " + str);
+
+        Stack<String> val = new Stack<>();
+        Stack<Character> op = new Stack<>();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                val.push(ch + "");
+
+            } else if (ch == ')') {
+                op.push(ch);
+
+            } else if (ch == '(') {
+                while (!op.isEmpty() && op.peek() != ')') {
+                    String v1 = val.pop();
+                    String v2 = val.pop();
+                    char o = op.pop();
+                    String t = o + v2 + v1;
+                    val.push(t);
+                }
+                op.pop(); // pop the ')'
+
+            } else {
+                while (!op.isEmpty() && precedence(op.peek()) > precedence(ch)) {
+                    String v1 = val.pop();
+                    String v2 = val.pop();
+                    char o = op.pop();
+                    String t = o + v2 + v1;
+                    val.push(t);
+                }
+                op.push(ch);
+            }
+        }
+
+        while (!op.isEmpty()) {
+            String v1 = val.pop();
+            String v2 = val.pop();
+            char o = op.pop();
+            String t = o + v2 + v1;
+            val.push(t);
+        }
+
+        System.out.println("Prefix Expression: " + val.peek());
+    }
+
+    public static int precedence(char op) {
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        return 0;
+    }
+}
