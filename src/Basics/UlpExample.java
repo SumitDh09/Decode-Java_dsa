6e198672f8613881f744270e623918ccf0a8b98f
dhond <dhondikarsumit@gmail.com>
2025-01-14 11:26:08 +0530

    All java Programs

---
+++ src/Basics/UlpExample.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class UlpExample
+{
+    public static void main(String[] args)
+    {
+        double a = 8.1;
+        // Input positive double value, Output ulp(a)
+        System.out.println(Math.ulp(a));
+    }
+}
