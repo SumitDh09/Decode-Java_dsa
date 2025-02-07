0095eb109b78f1fa3cfa21e9443f329bce33d0bd
dhond <dhondikarsumit@gmail.com>
2025-02-07 10:11:52 +0530

    All java Programs

---
+++ src/LeetcodeProblems/FindTheNumberOfDistinctColorsAmongTheBalls.java
@@ -0,0 +1,33 @@
+package LeetcodeProblems;
+import java.util.*;
+
+class FindTheNumberOfDistinctColorsAmongTheBalls {
+    public static void main(String[] args) {
+        FindTheNumberOfDistinctColorsAmongTheBalls solution = new FindTheNumberOfDistinctColorsAmongTheBalls();
+        int limit = 4;
+        int[][] queries = {{1, 4}, {2, 5}, {1, 3}, {3, 4}};
+        int[] result = solution.queryResults(limit, queries);
+
+        System.out.println(Arrays.toString(result)); // Output: [1, 2, 2, 3]
+    }
+
+    public int[] queryResults(int limit, int[][] queries) {
+        Map<Integer,Integer> ball = new HashMap<>(), color = new HashMap<>();
+        int n = queries.length, distinct = 0;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++){
+            int pos = queries[i][0], c = queries[i][1];
+            if(ball.containsKey(pos)){
+                int cnt = color.get(ball.get(pos)) - 1;
+                if(cnt == 0){ color.remove(ball.get(pos)); distinct--; }
+                else color.put(ball.get(pos), cnt);
+            }
+            ball.put(pos, c);
+            int cnt = color.getOrDefault(c, 0) + 1;
+            color.put(c, cnt);
+            if(cnt == 1) distinct++;
+            ans[i] = distinct;
+        }
+        return ans;
+    }
+}
